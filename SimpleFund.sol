3e333e8e88b89e3cab87c746bdb12b825ddbaf58
deuxbot <deuxbot@gmail.com>
2018-05-05 19:26:41 +0200

    Add SimpleFund

---
+++ SimpleFund.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.23;
+
+// Fund where anyone can deposit but only the owner can withdraw (real ether)
+contract SimpleFund {
+    uint private balance;
+    address private owner;
+    
+   constructor() public {
+        balance = 0;
+        owner = msg.sender;
+    }
+    
+    function getBalance() public view returns (uint) {
+        return balance;
+    }
+    
+    function deposit() public payable {
+        require(msg.sender.balance >= msg.value);
+        balance += msg.value;
+    }
+
+    function withdraw(uint ammount) public {
+        require(msg.sender == owner);
+        require(balance >= ammount);
+        balance -= ammount;
+        msg.sender.transfer(ammount);
+    }
+}
